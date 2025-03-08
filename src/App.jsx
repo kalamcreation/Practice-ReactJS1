5727fed9c4dbd95cda641c72191bb3f199aacd35
unknown <mdabulkalamosman@gmail.com>
2025-03-08 15:45:15 +0600

    Dynamic Data with API Call using UserEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
+import LoadUser from "./assets/LoadUser";
 import Image from "./Image";
 import WorkSample from "./WorkSample";
 
 export default function App(){
   const workData = [
-    {
+    {/*
       year:"2018-2022",
       experience:"B.Sc in Computer Science & Engr.",
       institute:"University of Asia Pacific",
-    },
+    */},
   ];
 
   return (
     <div>
-    {workData.map((work) => (
+    {/*workData.map((work) => (
         <WorkSample work={work} />
-      ))}
+      ))*/}
+      <LoadUser/>
     </div>
   );
 }
---
+++ src/assets/LoadUser.jsx
@@ -0,0 +1,13 @@
+import { useEffect } from "react";
+
+export default function LoadUser() {
+
+    useEffect(()=>{
+        // Could be GET or POST/PUT/PATCH/DELETE
+        fetch('https://dummyjson.com/test')
+        .then(res => res.json())
+        .then(console.log);
+        /* { status: 'ok', method: 'GET' } */
+    },[2]);
+  return <div>LoadUser</div>
+}
