b7256c30a043804c3e7881d988e088100bb35846
kalamcreation <mdabulkalamosman@gmail.com>
2025-03-29 00:01:34 +0600

    React Router DOM 6 Installation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState, useEffect } from 'react'
 import { Button, Card } from 'react-bootstrap'
 
@@ -30,47 +29,6 @@ function App() {
     </div>
     </>
   )
-=======
-import { use, useEffect, useState } from "react";
-import LoadUser from "./assets/LoadUser";
-import Image from "./Image";
-import User from "./User";
-import WorkSample from "./WorkSample";
-import Navbar from "./Navbar";
-
-export default function App(){
-  const workData = [
-    {/*
-      year:"2018-2022",
-      experience:"B.Sc in Computer Science & Engr.",
-      institute:"University of Asia Pacific",
-    */},
-  ];
-  const [user, setUser] = useState([]);
-
-  useEffect(() => {
-    fetch("https://dummyjson.com/users")
-    .then ((res)=> res.json())
-    .then ((data)=> setUser(data.users));
-  }, []);
-  console.log(user);
-  return (
-    <>
-    {/* <Navbar/>
-      <div className="flex gap-2 flex-wrap justify-center">
-      {user.map((user) => (
-        <User key={user.id} userInfo={user}/>
-      ))};
-    {/*workData.map((work) => (
-        <WorkSample work={work} />
-      ))*/}
-      
-    </div> */}
-
-      <Form />
-    </>
-  );
->>>>>>> f0856b8ed05e1826adaa821efd53fe3a71f12f62
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,29 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import{
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Home</div>,
+  },
+  {
+    path: "/login",
+    element: <div>Login Page</div>,
+  },
+  {
+    path: "/register",
+    element: <div>Register</div>,
+  },
+]);
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 )
