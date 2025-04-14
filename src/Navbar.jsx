8b27f0ac44492c52b8b0c2b1a2b56bdc98182f71
kalamcreation <mdabulkalamosman@gmail.com>
2025-04-14 22:42:51 +0600

    Link & Navlink Active

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <div>
+        {/* <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link> */}
+
+        <NavLink
+        to="/"
+        className={({ isActive }) => isActive ? "text-danger" : "" }
+        >
+        Home
+        </NavLink>
+        <NavLink
+        to="/login"
+        className={({ isActive }) => isActive ? "text-danger" : "" }
+        >
+        Login
+        </NavLink>
+        <NavLink
+        to="/register"
+        className={({ isActive }) => isActive ? "text-danger" : "" }
+        >
+        Register
+        </NavLink>
+
+    </div>
   )
 }
